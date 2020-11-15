54afdb30df78120926e57c87d834b5b2d72254fe
dev4choonii <dev4choonii@gmail.com>
2020-11-15 17:36:39 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const movieList = [
     {
         id : "1",
         movieTitle : "Emily in Paris",
-        movieThumbnail : "https://cdn.cinematerial.com/p/297x/045z1v1t/emily-in-paris-movie-poster-md.jpg?v=1598976376"
+        moviePoster : "https://cdn.cinematerial.com/p/297x/045z1v1t/emily-in-paris-movie-poster-md.jpg?v=1598976376",
+        rating : 4.7
     },
     {
         id : "2",
         movieTitle : "Vikings",
-        movieThumbnail : "https://images-na.ssl-images-amazon.com/images/I/51VP7qnSjWL._AC_.jpg"
+        moviePoster : "https://images-na.ssl-images-amazon.com/images/I/51VP7qnSjWL._AC_.jpg",
+        rating : 5.0
+
     },
     {
         id : "3",
         movieTitle : "Interstellar",
-        movieThumbnail : "https://images-na.ssl-images-amazon.com/images/I/61ApVtPce5L._AC_SY879_.jpg"
+        moviePoster : "https://images-na.ssl-images-amazon.com/images/I/61ApVtPce5L._AC_SY879_.jpg",
+        rating : 4.3
     }
 ];
 
-function Movie({movie}) {
+function Movie({movieTitle, moviePoster, rating}) {
     return (
         <div>
-            Title : {movie.movieTitle}<br/>
+            Title : {movieTitle}<br/>
+            Rate : {rating}<br/>
             Poster<br/>
-            <img src={movie.movieThumbnail} alt={movie.movieTitle}/>
+            <img src={moviePoster} alt={movieTitle} width="297px" height="440px"/>
         </div>
     );
 }
 
+Movie.propTypes = {
+    movieTitle : PropTypes.string.isRequired,
+    moviePoster : PropTypes.string.isRequired,
+    rating : PropTypes.number
+}
+
 function RenderMovie() {
     return (
-        movieList.map(movie => <Movie key={movie.id} movie={movie}/>)
+        movieList.map(movie => <Movie key={movie.id} movieTitle={movie.movieTitle} moviePoster={movie.moviePoster} rating={movie.rating}/>)
     )
 }
 
